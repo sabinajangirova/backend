fc9aecbcf544d4ee5052dac699d74276a49a7150
Sabina Jangirova <furiyayuhu@gmail.com>
2023-04-20 15:15:23 +0600

    update after offline

--- repairworks/views.py
+++ repairworks/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from users.permissions import isStationWorker
 from repairworks.serializer import RepairWorkSerializer
-
+from rest_framework.response import Response
+from rest_framework import status
 from .models import RepairWork
 
 class RepairWorksListViewSet(ListCreateAPIView):
     serializer_class = RepairWorkSerializer
-    
     permission_classes = [IsAuthenticated, isStationWorker]
-    # permission_classes = []
 
     def get_queryset(self):
         queryset = RepairWork.objects.all()
@@ -29,7 +29,6 @@ class RepairWorkViewSet(RetrieveUpdateDestroyAPIView):
     serializer = RepairWorkSerializer(queryset, many=True)
 
     permission_classes = [IsAuthenticated, isStationWorker]
-    # permission_classes = []
     
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
@@ -39,3 +38,38 @@ class RepairWorkViewSet(RetrieveUpdateDestroyAPIView):
 
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
+
+class RepairWorkCreateListViewSet(CreateAPIView):
+    serializer_class = RepairWorkSerializer
+    permission_classes = [IsAuthenticated, isStationWorker]
+
+    def create(self, request, *args, **kwargs):
+        repairworks = request.data["repairworks"]
+        new_repairworks = []
+        not_created_repairworks = []
+        for r in repairworks:
+            serializer = RepairWorkSerializer(data=r)
+            if not serializer.is_valid():
+                not_created_repairworks.append(r)
+            repairwork = serializer.create(serializer.validated_data)
+            new_repairworks.append(RepairWorkSerializer(repairwork))
+        return Response(new_repairworks, status=status.HTTP_201_CREATED)
+    
+class RepairWorkUpdateListViewSet(APIView):
+    permission_classes = [IsAuthenticated, isStationWorker]
+
+    def patch(self, request):
+        serializer = RepairWorkSerializer(data=request.data["repairworks"], many=True, partial=True)
+        if serializer.is_valid():
+            ids = [item['id'] for item in serializer.validated_data if item.get('id')]
+            objects = RepairWork.objects.filter(id__in=ids)
+            RepairWork.objects.exclude(id__in=ids).delete()
+            for obj in objects:
+                data = next(item for item in serializer.validated_data if item['id'] == obj.id)
+                serializer = RepairWorkSerializer(obj, data=data, partial=True)
+                if serializer.is_valid():
+                    serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,4 @@ import users.views
 urlpatterns=[
     path('register', users.views.RegistrationView.as_view(), name="register"),
     path('me', users.views.UserApi.as_view(), name="me"),
-    # path('api/login/', users.views.LoginViewSet.as_view(), name='login'),
-    # path('api/refresh/', users.views.RefreshViewSet.as_view(), name='refresh'),
-    # path('api/profile/', users.views.UserApi.as_view(), name='profile'),
-    # path('api/logout/', users.views.LogoutApi.as_view(), name='logout')
 ]
